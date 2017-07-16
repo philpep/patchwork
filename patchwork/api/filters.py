4e597464576a1a29b0c98478020fc047b4181451
Philippe Pepiot <phil@philpep.org>
2017-07-16 20:35:41 +0200

    REST: move SeriesFilter below StateFilter
    So the first can use the second in its declaration.
    
    Signed-off-by: Philippe Pepiot <phil@philpep.org>

--- patchwork/api/filters.py
+++ patchwork/api/filters.py
@@ -71,13 +71,6 @@ class ProjectMixin(FilterSet):
                             queryset=Project.objects.all())
 
 
-class SeriesFilter(ProjectMixin, TimestampMixin, FilterSet):
-
-    class Meta:
-        model = Series
-        fields = ('submitter', 'project')
-
-
 class CoverLetterFilter(ProjectMixin, TimestampMixin, FilterSet):
 
     class Meta:
@@ -108,6 +101,13 @@ class StateChoiceField(ModelChoiceField):
 class StateFilter(ModelChoiceFilter):
 
     field_class = StateChoiceField
+
+
+class SeriesFilter(ProjectMixin, TimestampMixin, FilterSet):
+
+    class Meta:
+        model = Series
+        fields = ('submitter', 'project')
 
 
 class PatchFilter(ProjectMixin, TimestampMixin, FilterSet):
